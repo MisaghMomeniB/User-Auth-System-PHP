6e7fc0becc48be60fe00b74fd943f40256f11d2b
MisaghMomeniB <jenabemisagh@gmail.com>
2025-05-07 20:25:08 +0330

    index php file

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php require 'includes/header.php'; ?>
+
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <h3>Welcome to User Auth System</h3>
+        </div>
+        <div class="card-body">
+            <p>Please <a href="login.php">login</a> or <a href="register.php">register</a> to continue.</p>
+        </div>
+    </div>
+</div>
+
+<?php require 'includes/footer.php'; ?>
